f1e5578fb527ce7e811cae5e5f452b601330445d
Jason Holt Smith <jsmith@kobo.com>
2015-02-10 18:07:48 +0000

    updating functional tests

--- test/func/testThree.js
+++ test/func/testThree.js
@@ -1,11 +1,11 @@
-PFT.tester.run("Test One - A", function (page, assert) {
+PFT.tester.run("Test Three - A", function (page, assert) {
     assert.isTrue(true, "Expected true");
     setTimeout(function () {
         assert.pass();
     }, 1000);
 });
 
-PFT.tester.run("Test One - B", function (page, assert) {
+PFT.tester.run("Test Three - B", function (page, assert) {
     assert.pass();
     does.not.exist = 1; // should not make it here
 });
